0c0a81ceca2f554f27ce792c81d411413d62ef1f
Chrisxavix <chrischillogallid64@outlook.com>
2024-07-22 22:50:10 -0500

    selectores - consume country and paint in drop

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Region } from '../../interfaces/country.interfaces';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'app-selector-page',
   templateUrl: './selector-page.component.html',
   styleUrl: './selector-page.component.css'
 })
-export class SelectorPageComponent {
+export class SelectorPageComponent implements OnInit{
+
+  constructor(
+    private fb: FormBuilder,
+    private countriesService: CountriesService,
+  ) {}
+
+  public regions: Region[] = [];
+
+  public myForm: FormGroup = this.fb.group({
+    region: ["", Validators.required],
+    country: ["", Validators.required],
+    borders: ["", Validators.required]
+  })
+
+  ngOnInit(): void {
+    this.regions = this.countriesService.getRegions();
+    console.log(this.regions, " brou");
+  }
+
 
 }
